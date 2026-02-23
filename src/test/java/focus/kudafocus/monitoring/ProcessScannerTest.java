b96dbf970fdde1f81a4494d92e8adf7030719076
Harry Jiang <eternalaxholo28@gmail.com>
2026-02-23 12:44:28 -0500

    chore: first iteration of process scanning

---
+++ src/test/java/focus/kudafocus/monitoring/ProcessScannerTest.java
@@ -0,0 +1,15 @@
+package focus.kudafocus.monitoring;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProcessScannerTest {
+    private static class FakeMonitor extends AppMonitor {
+    private List<ProcessInfo> list = new ArrayList<>();
+    @Override protected List<ProcessInfo> getCurrentProcesses() {
+        return new ArrayList<>(list);
+    }
+    @Override protected String normalizeProcessName(String raw) { return raw; }
+    public void setProcesses(List<ProcessInfo> procs) { list = new ArrayList<>(procs); }
+    }
+}
