89f8737c8d1b2e7db10292e11fadce538aabb540
Harry Jiang <eternalaxholo28@gmail.com>
2026-02-25 14:33:26 -0500

    test: update processscannertest.java

--- src/test/java/focus/kudafocus/monitoring/ProcessScannerTest.java
+++ src/test/java/focus/kudafocus/monitoring/ProcessScannerTest.java
@@ -5,11 +5,16 @@ import java.util.List;
 
 public class ProcessScannerTest {
     private static class FakeMonitor extends AppMonitor {
-    private List<ProcessInfo> list = new ArrayList<>();
-    @Override protected List<ProcessInfo> getCurrentProcesses() {
-        return new ArrayList<>(list);
-    }
-    @Override protected String normalizeProcessName(String raw) { return raw; }
-    public void setProcesses(List<ProcessInfo> procs) { list = new ArrayList<>(procs); }
+        private List<ProcessInfo> list = new ArrayList<>();
+        
+        @Override 
+        protected List<ProcessInfo> getCurrentProcesses() {
+            return new ArrayList<>(list);
+        }
+
+        @Override 
+        protected String normalizeProcessName(String raw) { return raw; }
+
+        public void setProcesses(List<ProcessInfo> procs) { list = new ArrayList<>(procs); }
     }
 }
